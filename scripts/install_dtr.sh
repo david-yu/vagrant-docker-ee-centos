abb13abc99e2a401fed0e72b1c14a94d93d7b0b6
David Yu <david.yu@docker.com>
2018-04-18 19:44:03 -0700

    changing username for dtr backup

--- scripts/install_dtr.sh
+++ scripts/install_dtr.sh
@@ -14,7 +14,7 @@ sleep 90
 # Install DTR
 sudo -E sh -c 'docker run --rm docker/dtr:${DTR_VERSION} install --ucp-url https://"${UCP_IPADDR}" --ucp-node dtr.local --replica-id "${DTR_REPLICA_ID}" --dtr-external-url https://dtr.local --ucp-username "${UCP_USERNAME}" --ucp-password "${UCP_PASSWORD}" --ucp-insecure-tls'
 # Run backup of DTR
-sudo -E sh -c 'docker run --rm --log-driver none docker/dtr:${DTR_VERSION} backup --ucp-url https://"${UCP_IPADDR}" --existing-replica-id "${DTR_REPLICA_ID}" --ucp-username admin --ucp-password "${UCP_PASSWORD}" --ucp-insecure-tls > /tmp/backup.tar'
+sudo -E sh -c 'docker run --rm --log-driver none docker/dtr:${DTR_VERSION} backup --ucp-url https://"${UCP_IPADDR}" --existing-replica-id "${DTR_REPLICA_ID}" --ucp-username "${UCP_USERNAME}" --ucp-password "${UCP_PASSWORD}" --ucp-insecure-tls > /tmp/backup.tar'
 
 # Sleep 30 seconds to wait for DTR to come back up
 sleep 30
